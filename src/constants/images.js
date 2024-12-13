8d38a50e399ba90fff885587e975a95a5794ae61
Joven Bandagosa <vhenbandagosa123@gmail.com>
2024-12-13 11:09:01 +0800

    feat: get started

--- src/constants/images.js
+++ src/constants/images.js
@@ -40,6 +40,31 @@ import dataframe5 from "../assets/images/dataframe5.png";
 import dataframe6 from "../assets/images/dataframe6.png";
 import dataframe7 from "../assets/images/dataframe7.png";
 
+import python1 from "../assets/images/python1.png";
+import python2 from "../assets/images/python2.png";
+import python3 from "../assets/images/python3.png";
+import python4 from "../assets/images/python4.png";
+import python5 from "../assets/images/python5.png";
+import python6 from "../assets/images/python6.png";
+
+import git1 from "../assets/images/git1.png";
+import git2 from "../assets/images/git2.png";
+import git3 from "../assets/images/git3.png";
+import git4 from "../assets/images/git4.png";
+import git5 from "../assets/images/git5.png";
+import git6 from "../assets/images/git6.png";
+import git7 from "../assets/images/git7.png";
+import git8 from "../assets/images/git8.png";
+import git9 from "../assets/images/git9.png";
+import git10 from "../assets/images/git10.png";
+import git11 from "../assets/images/git11.png";
+import git12 from "../assets/images/git12.png";
+import git13 from "../assets/images/git13.png";
+import git14 from "../assets/images/git14.png";
+import git15 from "../assets/images/git15.png";
+import git16 from "../assets/images/git16.png";
+import git17 from "../assets/images/git17.png";
+
 export {
   logo,
   main,
@@ -76,4 +101,27 @@ export {
   dataframe5,
   dataframe6,
   dataframe7,
+  python1,
+  python2,
+  python3,
+  python4,
+  python5,
+  python6,
+  git1,
+  git2,
+  git3,
+  git4,
+  git5,
+  git6,
+  git7,
+  git8,
+  git9,
+  git10,
+  git11,
+  git12,
+  git13,
+  git14,
+  git15,
+  git16,
+  git17,
 };
